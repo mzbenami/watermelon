d4b7613e8f38ea243416f5e2ef0bb0104597b7b1
Michael Ben-Ami <mike.benami@gmail.com>
2014-10-24 14:43:29 -0400

    chose better grid

--- src/watermelon/group4/Player.java
+++ src/watermelon/group4/Player.java
@@ -1,6 +1,7 @@
 package watermelon.group4;
 
 import java.util.*;
+import java.lang.Math;
 
 import watermelon.sim.Pair;
 import watermelon.sim.Point;
@@ -8,11 +9,18 @@ import watermelon.sim.seed;
 
 public class Player extends watermelon.sim.Player {
 	static double distowall = 1.00;
-	static double distotree = 1.00;
+	static double distotree = 2.00;
 	static double distoseed = 2.00;
 
-	public void init() {
+	static final int ALT_GRID_MOVE = 0;
+	static final int ALT_GRID_STAG_MOVE = 1;
+	static final int CHOOSE_ALT_GRID = 2;
 
+	private boolean hasInitialized = false;
+	private int ourMove;
+	private double s;
+
+	public void init() {
 	}
 
 	static double distance(seed tmp, Pair pair) {
@@ -25,8 +33,29 @@ public class Player extends watermelon.sim.Player {
 		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 	}
 
+
+	static double distanceseed(seed a, seed b) {
+		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+	}
+
 	@Override
 	public ArrayList<seed> move(ArrayList<Pair> treelist, double width, double length, double s) {
+		
+		if (!hasInitialized) {
+			this.s = s;
+			hasInitialized = true;
+		}
+
+		ArrayList<ArrayList<seed>> solutionList = new ArrayList<ArrayList<seed>>();
+
+		solutionList.add(altGridMove(treelist, width, length, s));
+		solutionList.add(staggeredMove(treelist, width, length, s));
+		return chooseAltGrid(solutionList);
+
+	}
+
+
+	public ArrayList<seed> altGridMove(ArrayList<Pair> treelist, double width, double length, double s) {
 		// TODO Auto-generated method stub
 
 		boolean lastime = false;
@@ -56,7 +85,90 @@ public class Player extends watermelon.sim.Player {
 			}
 
 		}
-		System.out.printf("seedlist size is %d", seedlist.size());
 		return seedlist;
+	}
+
+	public ArrayList<seed> staggeredMove(ArrayList<Pair> treelist, double width, double length, double s) {
+		// TODO Auto-generated method stub
+
+		boolean lastime = false;
+
+		ArrayList<seed> seedlist = new ArrayList<seed>();
+		for (double i = distowall; i < width - distowall; i = i + distoseed) {
+			int rowCounter = 0;
+			for (double j = distowall; j < length - distowall; j = j + Math.tan(Math.toRadians(60.001))) {
+				seed tmp;
+				double stag_i = (rowCounter % 2 == 0) ? i : i + 1;
+				tmp = new seed(stag_i, j, lastime);
+				boolean add = true;
+				for (int f = 0; f < treelist.size(); f++) {
+					if (distance(tmp, treelist.get(f)) < distotree) {
+						add = false;
+						break;
+					}
+				}
+				if (add) {
+					seedlist.add(tmp);
+				}
+				
+				lastime = !lastime;
+				rowCounter++;
+			}
+			if (rowCounter % 2 == 0) {
+				lastime = !lastime;
+			}
+
+		}
+		return seedlist;
+	}
+
+	public ArrayList<seed> chooseAltGrid(ArrayList<ArrayList<seed>> solutionList) {
+		double highScore = 0.0;
+		double temp = 0.0;
+		ArrayList<seed> finalList = null;
+		for (ArrayList<seed> solution : solutionList){
+			temp = calculatescore(solution);
+			if (temp > highScore){
+				highScore = temp;
+				finalList = solution;
+			}
+		}
+		
+		return finalList;
+	}
+
+	private double calculatescore(ArrayList<seed> seedlist) {
+		double total = 0.0;
+
+		for (int i = 0; i < seedlist.size(); i++) {
+			double score;
+			double chance = 0.0;
+			double totaldis = 0.0;
+			double difdis = 0.0;
+			for (int j = 0; j < seedlist.size(); j++) {
+				if (j != i) {
+					totaldis = totaldis
+							+ Math.pow(
+									distanceseed(seedlist.get(i),
+											seedlist.get(j)), -2);
+				}
+			}
+			for (int j = 0; j < seedlist.size(); j++) {
+				if (j != i
+						&& ((seedlist.get(i).tetraploid && !seedlist.get(j).tetraploid) || (!seedlist
+								.get(i).tetraploid && seedlist.get(j).tetraploid))) {
+					difdis = difdis
+							+ Math.pow(
+									distanceseed(seedlist.get(i),
+											seedlist.get(j)), -2);
+				}
+			}
+			//System.out.println(totaldis);
+			//System.out.println(difdis);
+			chance = difdis / totaldis;
+			score = chance + (1 - chance) * s;
+			total = total + score;
+		}
+		return total;
 	}
 }
