f9c1aa0441d41a7a72170120d9de9b3c67b6e243
powerrmt <powerrmt@gmail.com>
2014-11-03 14:11:11 -0500

    Added another field to seed class

--- src/watermelon/sim/seed.java
+++ src/watermelon/sim/seed.java
@@ -8,13 +8,14 @@ public class seed {
     public double y;
     public boolean tetraploid;
     public double score;
+    public boolean marked;
     
-    public seed() { x = 0.0; y = 0.0; tetraploid = false; }
+    public seed() { x = 0.0; y = 0.0; tetraploid = false; marked = false;}
 
     public seed(double xx, double yy, boolean tetra) {
         x = xx;
         y = yy;
         tetraploid = tetra;
-       
+        marked = false;
     }
 }
